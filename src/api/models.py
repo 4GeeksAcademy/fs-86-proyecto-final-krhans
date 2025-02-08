a48463e626ef5eb6337667a6dab23be227b78b9e
Cristian-svg598 <criguies@gmail.com>
2025-02-08 08:55:49 +0000

    Creacion modelo user_profile

--- src/api/models.py
+++ src/api/models.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from argon2 import PasswordHasher
 
-
 db = SQLAlchemy()
 ph = PasswordHasher()
 
@@ -12,6 +11,13 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
     is_active = db.Column(db.Boolean(), nullable=False, default=False)
+    
+    profile = db.relationship(
+        'User_profile', 
+        uselist=False, 
+        back_populates='user',
+        primaryjoin="User.id == User_profile.user_id"  
+    )
 
     def set_password(self, password):
         self.password_hash = ph.hash(password)
@@ -32,4 +38,14 @@ class User(db.Model):
             "email": self.email,
             "is_active": self.is_active
         }
+
+
+class User_profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
+    user = db.relationship(
+        'User', 
+        back_populates='profile',
+        primaryjoin="User.id == User_profile.user_id" 
+    )
